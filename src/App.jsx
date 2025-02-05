805010bbb33979296106b93dba1f3252e901d3fa
YJCHO <knoo13556@gmail.com>
2025-02-05 15:48:50 +0900

    react map

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,48 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import PrivateRoute from './routes/PrivateRoute';
-import { MainPage } from './pages/MainPage';
-import { Login } from './pages/Login';
-import './App.css'
+// PageController 메뉴별 페이지
 
-function App() {
+// import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+// import PrivateRoute from './routes/PrivateRoute';
+// import { MainPage } from './pages/MainPage';
+// import { Login } from './pages/Login';
+// import './App.css'
 
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<PrivateRoute />}>
-            <Route path="/" element={<MainPage />} />
-          </Route>
-        </Routes>
-      </Router>
-    </div>
-  )
-}
+// function App() {
 
-export default App
+//   return (
+//     <div>
+//       <Router>
+//         <Routes>
+//           <Route path="/login" element={<Login />} />
+//           <Route element={<PrivateRoute />}>
+//             <Route path="/" element={<MainPage />} />
+//           </Route>
+//         </Routes>
+//       </Router>
+//     </div>
+//   )
+// }
 
-// import React, { Suspense } from "react";
-// // React.lazy로 동적 로딩
+// export default App
+
+
+
+// React.lazy로 동적 로딩
+import React, { Suspense } from "react";
+import "./assets/css/base.css"
+
 
 // const MainPage = React.lazy(() => import("./pages/MainPage"));
 // const Logins = React.lazy(() => import("./pages/Login"));
+const ReactMap = React.lazy(() => import("./features/gis/ReactMap"));
 
-// const App = () => {
-//   return (
-//     <div>
-//       <Suspense fallback={<div>Loading...</div>}>
-//         <MainPage />
-//         {/* <Logins /> */}
-//       </Suspense>
-//     </div>
-//   );
-// };
+const App = () => {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ReactMap />
+      </Suspense>
+    </div>
+  );
+};
 
-// export default App;
+export default App;
--- src/features/gis/ReactMap.jsx
+++ src/features/gis/ReactMap.jsx
@@ -1,3 +1,4 @@
+import { changeDraw, changeLayer, changeMeasure, changeZoom, endDraw, removeSource } from "../../function/MapFunc";
 import { useEffect, useRef } from "react";
 import { useMapStore } from "../../stores/MapStore";
 import "../../assets/css/map.css"
@@ -6,20 +7,79 @@ const ReactMap = () => {
 
     const map = useMapStore(a => a.map);
     const mapRef = useRef();
+    const Ref = useRef(null);
 
     useEffect(() => {
-        map.setTarget();
-        if (!mapRef.current) return
-        console.log(map);
-        map.setTarget(mapRef.current);
-        map.on('click', function (e) {
-            console.log(e.coordinate);
-        })
+        if (mapRef.current) {
+            map.setTarget();
+            map.setTarget(mapRef.current);
+            map.on('click', function (e) {
+                console.log(e.coordinate);
+            })
+        }
     }, [])
+
+    const selectHandler = (type, value) => {
+
+        switch (type) {
+            case "endDraw":
+                endDraw();
+                Ref.current.querySelector('[id ="draw_select"]').value = "None";
+                Ref.current.querySelector('[id ="measure_select"]').value = "None";
+                break
+            case "changeDraw":
+                changeDraw(value)
+                Ref.current.querySelector('[id = "measure_select"]').value = "None";
+                break
+            case "changeMeasure":
+                changeMeasure(value);
+                Ref.current.querySelector('[id = "draw_select"]').value = "None";
+                break
+            case "removeSource":
+                removeSource(value);
+                Ref.current.querySelector('[id ="draw_select"]').value = "None";
+                Ref.current.querySelector('[id ="measure_select"]').value = "None";
+                break
+            default:
+                break
+        }
+
+    }
+    const test = () => {
+        console.log(map.getView().getZoom());
+        console.log(mapRef);
+        debugger
+    }
 
     return (
         <>
-            <div ref={mapRef} className="main_gis" />
+            <div>
+                <div ref={mapRef} className="map" />
+                {/* <div ref={Ref}>
+                    <div id="mapbtns">
+                        <button onClick={() => selectHandler("endDraw")}>그리기 종료</button>
+                        <button onClick={() => selectHandler("removeSource", "drawLayer")}>그리기 지우기</button>
+                        <button onClick={() => selectHandler("removeSource", "measureLayer")}>측정 지우기</button>
+                        <button onClick={() => changeZoom()}>center</button>
+                        <button id="osmLayer" onClick={() => changeLayer("osmLayer")}>OSMLayer</button>
+                        <button id="vworldLayer" onClick={() => changeLayer("vworldLayer")}>VWorldLayer</button>
+                        <button onClick={test}>test</button>
+                    </div>
+                    <div id="mapbtns2">
+                        <select id="draw_select" onChange={(e) => selectHandler("changeDraw", e.target.value)}>
+                            <option value="None">그리기</option>
+                            <option value="Point">점</option>
+                            <option value="LineString">선</option>
+                            <option value="Polygon">면</option>
+                        </select>
+                        <select id="measure_select" onChange={(e) => selectHandler("changeMeasure", e.target.value)}>
+                            <option value="None">측정</option>
+                            <option value="LineString">선</option>
+                            <option value="Polygon">면</option>
+                        </select>
+                    </div >
+                </div> */}
+            </div>
         </>
     )
 
